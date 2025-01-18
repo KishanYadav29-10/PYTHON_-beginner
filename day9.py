3eec15fc4bf374780211be62231b5b12cdd6c842
Kishan Yadav <kishanyadav.29102002@gmail.com>
2025-01-18 19:36:38 +0530

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,43 @@
+# datatyoes in python 
+i = 4
+print(type(i))
+
+f = 110.70
+print(type(f))
+
+f1 = 3e0
+print(f1)
+
+f2 = 3e1
+print(f2)
+
+f3=3e3
+print(f3)
+
+f4= 2.3e4
+print(f4)
+
+True
+False
+None
+
+print(True + False) #+ None
+print(True + False*True-False) 
+print(True + (False*True)-False) 
+a = True + (False*True)-False
+print(a) 
+print(True + (False*True)-True) 
+print(True + True)
+
+num1 = 20
+num2 = 30
+add = num1+num2
+print(f'the value of {num1} and {num2} is {add}')
+
+c = 10 + 20j  #in complex only j letter is allowed for imaginary numbers
+print(c)
+print(c.real)
+
+d = 20 + 30j
+print(d.imag)
+print(c+d)
